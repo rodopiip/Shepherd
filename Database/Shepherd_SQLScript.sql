eeb20fead06c26865597b5970e7132d3482434fd
rodopiip <mariaradeva9749@gmail.com>
2023-06-04 18:46:30 +0300

    dependencies -> pom.xml; fixed database

--- Database/Shepherd_SQLScript.sql
+++ Database/Shepherd_SQLScript.sql
@@ -5,70 +5,58 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `shepherd` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
 -- Schema shepherd
 -- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema travellerdb
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `shepherd` DEFAULT CHARACTER SET utf8mb3 ;
+
+USE `shepherd` ;
 
 -- -----------------------------------------------------
--- Schema travellerdb
--- -----------------------------------------------------
-
-
--- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `shepherd`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `shepherd`.`users` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tasks`
+-- Table `shepherd`.`tasks`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `shepherd`.`tasks` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
   `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `mydb`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `shepherd`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tunes`
+-- Table `shepherd`.`tunes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `shepherd`.`tunes` (
   `id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
   `url` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`alarms`
+-- Table `shepherd`.`alarms`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `shepherd`.`alarms` (
   `id` INT NOT NULL,
-  `description` VARCHAR(45) NULL,
+  `description` VARCHAR(45) NULL DEFAULT NULL,
   `time` DATETIME NOT NULL,
   `task_id` INT NOT NULL,
   `tune_id` INT NOT NULL,
@@ -77,15 +65,12 @@ CREATE TABLE IF NOT EXISTS `shepherd`.`alarms` (
   INDEX `tune_id_idx` (`tune_id` ASC) VISIBLE,
   CONSTRAINT `task_id`
     FOREIGN KEY (`task_id`)
-    REFERENCES `mydb`.`tasks` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+    REFERENCES `shepherd`.`tasks` (`id`),
   CONSTRAINT `tune_id`
     FOREIGN KEY (`tune_id`)
-    REFERENCES `mydb`.`tunes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `shepherd`.`tunes` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
---
+++ Shepherd_BE/src/main/java/com/example/shepherd_be/ShepherdBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.shepherd_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShepherdBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShepherdBeApplication.class, args);
+	}
+
+}
